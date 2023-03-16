2ceb3cb7fbbc1e907f1242a1859d4a39643696ff
Dylan Chew <lchew@student.42kl.edu.my>
2023-03-16 19:23:14 +0800

    [Bugfix] Fix main function returning 1 & 2. To pass tester it should return 0

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lchew <lchew@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 13:49:38 by lchew             #+#    #+#             */
-/*   Updated: 2023/03/16 15:01:45 by lchew            ###   ########.fr       */
+/*   Updated: 2023/03/16 18:59:53 by lchew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int	main(int argc, char *argv[])
 	t_stack	stack;
 
 	if (argc < 2)
-		return (1);
+		return (0);
 	stack_init(&stack);
 	if (argc == 2)
 	{
 		if (argv[1][0] == '\0')
-			return (2);
+			return (0);
 		stack.str_arg = ft_split(*(++argv), ' ');
 		insert_arg(&stack, 1, stack.str_arg);
 		free2d(stack.str_arg);
